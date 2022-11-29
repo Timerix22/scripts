2584587a0f3605e94e8d99a7dfe759b8c261df00
timerix-server <timerix11@gmail.com>
2022-11-30 01:41:19 +0300

    scripts from vps

---
+++ system_clean.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+df /home
+# apt cache
+sudo apt clean
+dpkg --list |grep "^rc" | cut -d " " -f 3 | xargs sudo dpkg --purge
+# logs
+sudo rm -rf /var/logs/*
+# temp
+rm -rf /tmp/*
+# nuget
+rm -rf ~/.nuget
+rm -rf ~/.local/share/NuGet
+# unused locales
+sudo localepurge
+df /home
