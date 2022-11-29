eca9c9feaf33f86f54ce759a390ab3d21f86c53e
timerix <timerix11@gmail.com>
2022-11-30 04:48:39 +0600

    ffmpeg_dir_convert warning

--- ffmpeg_dir_convert.sh
+++ ffmpeg_dir_convert.sh
@@ -4,6 +4,7 @@ if [ "$1" = "help" ] || [ "$1" = "h" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ]
 	echo $1
 	echo "[DIR SRC] [SRC FILE FORMAT] [TARGET DIR] [TARGET FORMAT]"
 	echo "EXAMPLE: ffmpeg_dir_convert.sh downloads ogg music/new mp3"
+	echo "!!! file names shouldnt contain spaces !!!"
 	exit 0
 fi
 DIN="$1"
