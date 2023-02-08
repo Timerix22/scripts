4981dad20bce6263f9cd17534556c92c56674486
timerix <timerix11@gmail.com>
2023-02-08 15:30:11 +0600

    mkcd

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ There i store some useful bash scripts from all my linux machines.
 ```bash
 git clone https://github.com/Timerix22/scripts.git
 echo "export PATH=$PATH:~/scripts" >> .path'
-ln -s scripts/.allrc ~/
+ln -s $pwd/scripts/.allrc ~/
 echo "source ~/.allrc" >> ~/.zshrc
+echo 'PATH=$PATH:'$pwd/scripts >> .path
 ```
--- ffmpeg_dir_convert.sh
+++ ffmpeg_dir_convert.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
-if [ "$1" = "help" ] || [ "$1" = "h" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$1" = "/?" ]; then
-	echo $1
-	echo "[DIR SRC] [SRC FILE FORMAT] [TARGET DIR] [TARGET FORMAT]"
+if [ $# -eq 0 ] || [ "$1" = "help" ] || [ "$1" = "h" ] || [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$1" = "/?" ]; then
+	echo "usage: ffmpeg_dir_convert.sh [DIR_SRC] [SRC_FILE_EXT] [TARGET_DIR] [TARGET_FILE_EXT]"
 	echo "EXAMPLE: ffmpeg_dir_convert.sh downloads ogg music/new mp3"
-	echo "!!! file names shouldnt contain spaces !!!"
+	echo "!!! file names must not contain spaces !!!"
 	exit 0
 fi
 DIN="$1"
