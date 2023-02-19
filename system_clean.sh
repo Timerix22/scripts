809101c1f017272dd30ad5a5ecead59ccd48e58f
Timerix22 <timerix11@gmail.com>
2023-02-20 00:37:00 +0600

    pacman_autoremove

--- system_clean.sh
+++ system_clean.sh
@@ -1,8 +1,31 @@
 #!/usr/bin/bash
 df -h /home
-# apt cache
-sudo apt clean
-dpkg --list |grep "^rc" | cut -d " " -f 3 | xargs sudo dpkg --purge
+
+function is_bin_in_path {
+  if [[ -n $ZSH_VERSION ]]; then
+    builtin whence -p "$1" &> /dev/null
+  else  # bash:
+    builtin type -P "$1" &> /dev/null
+  fi
+}
+
+function clean_apt {
+    echo "cleaning apt cache"
+    sudo apt autoremove
+    sudo apt clean
+    dpkg --list |grep "^rc" | cut -d " " -f 3 | xargs sudo dpkg --purge
+}
+
+function clean_pacman {
+    echo "cleaning pacman cache"
+    pacman_autoremove
+    pacman -Sc
+    #pacman -Scc
+}
+
+is_bin_in_path apt && clean_apt
+is_bin_in_path pacman && clean_pacman
+
 # logs
 sudo rm -rf /var/logs/*
 # temp
