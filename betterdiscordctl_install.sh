eeaecfb2d65cdf5282166bf6471fab0c11003293
Timerix22 <timerix11@gmail.com>
2023-02-20 00:39:24 +0600

    betterdiscordctl_install

---
+++ betterdiscordctl_install.sh
@@ -0,0 +1,4 @@
+curl -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/master/betterdiscordctl
+chmod +x betterdiscordctl
+sudo mv betterdiscordctl /usr/local/bin
+betterdiscordctl install
